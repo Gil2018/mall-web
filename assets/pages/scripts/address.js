0717a6fbce677d8db557e870a2fe1565ad565f5f
zct <dearfriendz@163.com>
2019-07-17 17:50:29 +0800

    新增收货地址,结算

--- assets/pages/scripts/address.js
+++ assets/pages/scripts/address.js
@@ -1,63 +1,4 @@
 
-
-function handleCreate(e) {
-    const flag = validform().form()
-    if (flag) {
-        let location = $(e).attr('location')
-        if (location) {
-            console.log(location)
-        } else {
-            $.alert({
-                title: '',
-                content: '请输入关键词查找地址!',
-            });
-            $('#keywords').focus()
-        }
-    }
-}
-
-function validform() {
-    return $("#edit-address").validate({
-        rules: {
-            fullName: {
-                required: true,
-                minlength: 4,
-                maxlength: 25
-            },
-            mobile: {
-                required: true,
-                minlength: 11,
-                isMobile: true
-            },
-            addressName: {
-                required: true,
-            },
-            addressDetail: {
-                required: true
-            }
-        },
-        messages: {
-            fullName: {
-                required: "收货人姓名不能为空",
-                minlength: "姓名不能少于4个字符(1个汉字代表2字符)",
-                maxlength: "密码长度不能超过25个字符"
-            },
-            mobile: {
-                required: "请输入联系电话",
-                minlength: "不能小于11个字符",
-                isMobile: "请正确填写手机号码"
-            },
-            addressName: {
-                required: "收货地址不能为空",
-            },
-            addressDetail: {
-                required: "详细地址不能为空"
-            }
-        }
-    });
-}
-
-
 var $table;
 const token = sessionStorage.getItem('token-wx')
 //初始化bootstrap-table的内容
@@ -69,6 +10,7 @@ function InitMainTable() {
             headers: { "token-wx": token }
         },
         method: 'GET',                      //请求方式（*）
+        dataType: 'json',
         //toolbar: '#toolbar',              //工具按钮用哪个容器
         striped: true,                      //是否显示行间隔色
         cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
@@ -76,8 +18,8 @@ function InitMainTable() {
         sortable: false,                     //是否启用排序
         sidePagination: "client",           //分页方式：client客户端分页，server服务端分页（*）
         pageNumber: 1,                      //初始化加载第一页，默认第一页,并记录
-        pageSize: 10,                     //每页的记录行数（*）
-        pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+        // pageSize: 10,                     //每页的记录行数（*）
+        // pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
         search: false,                      //是否显示表格搜索
         strictSearch: true,
         // showColumns: false,                  //是否显示所有的列（选择显示的列）
@@ -96,8 +38,9 @@ function InitMainTable() {
             return temp;
         },
         columns: [{
-            checkbox: true,
-            visible: true                  //是否显示复选框
+            field: 'id',
+            title: 'ID',
+            sortable: true
         }, {
             field: 'name',
             title: '收货人姓名',
@@ -106,18 +49,22 @@ function InitMainTable() {
             field: 'mobile',
             title: '联系电话'
         }, {
-            field: 'addressName',
-            title: '收货地址'
+            field: 'addressDetail',
+            title: '所在城市',
+            formatter: addressFormatter
         }, {
             field: 'addressDetail',
-            title: '详细地址',
-            formatter: addressFormatter
+            title: '详细地址'
+        }, {
+            field: 'addressName',
+            title: '收货地址'
         }, {
             field: 'location',
             title: '经纬度'
         }, {
             field: 'isDefault',
-            title: '是否默认'
+            title: '是否默认',
+            formatter: defaultFormatter
         }, {
             field: 'ID',
             title: '操作',
@@ -127,7 +74,7 @@ function InitMainTable() {
             formatter: actionFormatter
         },],
         onLoadSuccess: function (res) {
-            console.log(res)
+            $('#address-table').bootstrapTable('load', res.data)
         },
         onLoadError: function () {
             // showTips("数据加载失败！");
@@ -139,97 +86,214 @@ function InitMainTable() {
     });
 };
 
+// 地址格式化
 function addressFormatter(value, row, index) {
-    return `<span>${row.province}-${row.city}-${row.district}-${row.addressDetail}</span>`
+    return `<span>${row.province}-${row.city}-${row.district}</span>`
+}
+
+// 是否为默认地址
+function defaultFormatter(value, row, index) {
+    var result = "";
+    if (row.isDefault) {
+        result = `<span class="address-default">默认地址</span>`
+    } else {
+        result = `<span class="address-setDefault" onclick="handleSetDefault(this)">设为默认<span style="display: none;">${JSON.stringify(row)}</span></span>`
+    }
+    return result;
+}
+
+// 设置为默认地址
+function handleSetDefault(e) {
+    let row = JSON.parse(e.children[0].innerText)
+    row.isDefault = true
+    ajaxPromise({ type: 'post', contentType: 'application/json', url: `${baseUrl}/wx/address/save`, data: JSON.stringify(row) }).then(res => {
+        ShowNotice(res.errmsg, '设置成功', 'success', 2000, true)
+        $('#address-table').bootstrapTable('refresh');
+    })
 }
 
 //操作栏的格式化
 function actionFormatter(value, row, index) {
     var id = value;
     var result = "";
-    result += "<a href='javascript:;' class='btn btn-xs green' onclick=\"EditViewById('" + id + "', view='view')\" title='查看'><span class='glyphicon glyphicon-search'></span></a>";
-    result += "<a href='javascript:;' class='btn btn-xs blue' onclick=\"EditViewById('" + id + "')\" title='编辑'><span class='glyphicon glyphicon-pencil'></span></a>";
-    result += "<a href='javascript:;' class='btn btn-xs red' onclick=\"DeleteByIds('" + id + "')\" title='删除'><span class='glyphicon glyphicon-remove'></span></a>";
-
+    result += "<a href='javascript:;' class='btn btn-xs blue' onclick=\"handleUpdate('" + row.id + "')\" title='编辑'><span class='glyphicon glyphicon-pencil'></span></a>";
+    result += "<a href='javascript:;' class='btn btn-xs red' onclick=\"handleDelete('" + row.id + "')\" title='删除'><span class='glyphicon glyphicon-remove'></span></a>";
     return result;
 }
+
 InitMainTable()
 
-// handleCreate() {
-//     this.temp = {
-//       id: undefined,
-//       categoryId: undefined,
-//       isOnSale: false,
-//       name: '',
-//       number: 0,
-//       retailPrice: 0,
-//       brief: '',
-//       keywords: '',
-//       supperSn: ''
-//     }
-//     this.picUrl = []
-//     this.keywords = []
-//     this.getCategory()
-//     this.dialogStatus = 'create'
-//     this.dialogFormVisible = true
-//     this.$nextTick(() => {
-//       this.$refs['dataForm'].clearValidate()
-//     })
-//   },
-//   createData() {
-//     this.temp.name = this.temp.name.replace(/(^\s*)|(\s*$)/g, '')
-//     this.temp.supperSn = this.temp.supperSn.replace(/(^\s*)|(\s*$)/g, '')
-//     this.$refs['dataForm'].validate((valid) => {
-//       if (valid) {
-//         if (promptMessage(Array, this.picUrl, '上传菜品图片')) {
-//           this.temp.picUrl = this.picUrl[0].url
-//           addSupper(this.temp).then((res) => {
-//             this.list.unshift(res.data.data)
-//             this.dialogFormVisible = false
-//             this.$notify({
-//               title: '成功',
-//               message: '创建成功',
-//               type: 'success',
-//               duration: 2000
-//             })
-//           })
-//         }
-//       }
-//     })
-//   },
-//   handleUpdate(row) {
-//     this.getCategory()
-//     this.getDetail(row.id)
-//     this.dialogStatus = 'update'
-//     this.dialogFormVisible = true
-//     this.$nextTick(() => {
-//       this.$refs['dataForm'].clearValidate()
-//     })
-//   },
-//   updateData() {
-//     this.temp.name = this.temp.name.replace(/(^\s*)|(\s*$)/g, '')
-//     this.temp.supperSn = this.temp.supperSn.replace(/(^\s*)|(\s*$)/g, '')
-//     this.$refs['dataForm'].validate((valid) => {
-//       if (valid) {
-//         if (promptMessage(Array, this.picUrl, '上传菜品图片')) {
-//           this.temp.picUrl = this.picUrl[0].url
-//           editSupper(this.temp).then((res) => {
-//             for (const v of this.list) {
-//               if (v.id === this.temp.id) {
-//                 const index = this.list.indexOf(v)
-//                 this.list.splice(index, 1, this.temp)
-//                 break
-//               }
-//             }
-//             this.dialogFormVisible = false
-//             this.$notify({
-//               title: '成功',
-//               message: '修改成功',
-//               type: 'success',
-//               duration: 2000
-//             })
-//           })
-//         }
-//       }
-//     })
-//   },
+// 保存数据
+function createData(e) {
+    const flag = validform().form()
+    if (flag) {
+        let location = $(e).attr('location')
+        if (location) {
+            // let gender = $("#gender option:selected").val()
+            let msg = '添加成功'
+            let params = {
+                addressDetail: $('#addressDetail').val().trim(),
+                addressName: $('#addressName').val().trim(),
+                city: $('#city').val(),
+                district: $('#district').val(),
+                isDefault: $("#isDefault[type='checkbox']").is(':checked'),
+                location: location,
+                mobile: $('#mobile').val().trim(),
+                name: $('#fullName').val().trim(),
+                province: $('#province').val()
+            }
+            let addressId = $('#address-save').attr('addressId')
+            if (addressId) {
+                params.id = addressId
+                msg = '修改成功'
+            }
+            ajaxPromise({ type: 'post', contentType: 'application/json', url: `${baseUrl}/wx/address/save`, data: JSON.stringify(params) }).then(res => {
+                ShowNotice(res.errmsg, msg, 'success', 2000, true)
+                if (addressId) {
+                    $('#address-table').bootstrapTable('refresh');
+                } else {
+                    let index = $('#address-table').bootstrapTable('getData').length;
+                    $('#address-table').bootstrapTable('insertRow', {
+                        index: index,
+                        row: res.data
+                    });
+                }
+                $('#address-form').modal('hide')
+            })
+        } else {
+            $.alert({
+                title: '',
+                content: '请输入关键词查找地址!',
+            });
+            $('#keywords').focus()
+        }
+    }
+}
+
+// 新增收货地址
+function handleCreate() {
+    resetTemp({
+        addressDetail: '',
+        addressName: '',
+        isDefault: false,
+        mobile: '',
+        name: '',
+        city: '',
+        district: '',
+        province: ''
+    })
+    $('#address-save').attr('addressId', '')
+    $('#addressFormLabel').text('新增地址')
+}
+
+// 初始化模态框
+function resetTemp(data) {
+    console.log(data)
+    $('#addressDetail').val(data.addressDetail)
+    $('#addressName').val(data.addressName)
+    $("#isDefault").prop("checked", data.isDefault);
+    $('#mobile').val(data.mobile)
+    $('#fullName').val(data.name)
+    $('#keywords').val('')
+    // $("#gender option:selected").val(data.gender)
+    if (!data.district || data.district === '') {
+        $('#distpicker').distpicker('reset');
+    } else {
+        $('#distpicker').distpicker('destroy');
+        $('#distpicker').distpicker({
+            province: data.province,
+            city: data.city,
+            district: data.district
+        });
+    }
+}
+
+// 表单验证
+function validform() {
+    return $("#edit-address").validate({
+        rules: {
+            fullName: {
+                required: true,
+                minlength: 2,
+                maxlength: 20
+            },
+            mobile: {
+                required: true,
+                minlength: 11,
+                isMobile: true
+            },
+            addressName: {
+                required: true,
+            },
+            addressDetail: {
+                required: true
+            }
+        },
+        messages: {
+            fullName: {
+                required: "收货人姓名不能为空",
+                minlength: "姓名不能少于2个字符",
+                maxlength: "姓名不能超过20个字符"
+            },
+            mobile: {
+                required: "请输入联系电话",
+                minlength: "不能小于11个字符",
+                isMobile: "请正确填写手机号码"
+            },
+            addressName: {
+                required: "收货地址不能为空",
+            },
+            addressDetail: {
+                required: "详细地址不能为空"
+            }
+        }
+    });
+}
+
+// 修改收货地址
+function handleUpdate(id) {
+    ajaxPromise({ url: `${baseUrl}/wx/address/detail`, data: { id: id } }).then(res => {
+        let data = res.data
+        resetTemp({
+            addressDetail: data.addressDetail,
+            addressName: data.addressName,
+            isDefault: data.isDefault,
+            mobile: data.mobile,
+            name: data.name,
+            city: data.city,
+            district: data.district,
+            province: data.province
+        })
+        $('#addressFormLabel').text('修改地址')
+        $('#address-save').attr('addressId', id)
+        $('#address-save').attr('location', data.location)
+        $('#address-form').modal()
+    })
+}
+
+// 删除收货地址
+function handleDelete(id) {
+    $.confirm({
+        title: '删除!',
+        content: '确认删除该条地址吗?',
+        buttons: {
+            cancel: {
+                text: '取消'
+            },
+            confirm: {
+                text: '确认',
+                btnClass: 'btn-blue',
+                keys: ['enter', 'shift'],
+                action: function () {
+                    ajaxPromise({ type: 'post', contentType: 'application/json', url: `${baseUrl}/wx/address/delete`, data: JSON.stringify({ id: id }) }).then(res => {
+                        ShowNotice(res.errmsg, '删除成功', 'success', 2000, true)
+                        $('#address-table').bootstrapTable('remove', {
+                            field: "id",
+                            values: [parseInt(id)]
+                        });
+                    })
+                }
+            }
+        }
+    });
+}
--- assets/pages/scripts/checkout.js
+++ assets/pages/scripts/checkout.js
@@ -1,52 +1,109 @@
 var Checkout = function () {
-
   return {
     init: function () {
-
       $('#checkout').on('change', '#checkout-content input[name="account"]', function () {
-
         var title = '';
-
         if ($(this).attr('value') == 'register') {
           title = 'Step 2: Account &amp; Billing Details';
         } else {
           title = 'Step 2: Billing Details';
         }
-
         $('#payment-address .accordion-toggle').html(title);
       });
-
     }
   };
-
 }();
 
-$('.address-list label.addressInfo').click(function () {
-  const that = this
-  $.confirm({
-    title: '地址!',
-    content: '更换地址后, 您需要重新确认订单信息',
-    buttons: {
-      cancel: {
-        text: '取消'
-      },
-      confirm: {
-        text: '确认',
-        btnClass: 'btn-blue',
-        keys: ['enter', 'shift'],
-        action: function () {
-          $('.address-wrap').attr('class', 'address-wrap')
-          $(that).parent().parent().addClass('default selected')
-          $('.address-wrap .addressBox .marker-par').remove()
-          $(that).before(`<div class="marker-par">
-          <i class="fa fa-location-arrow marker"></i>
-          <span class="marker-tip">寄送至</span>
-        </div>`)
-          // console.log($(that).parent().parent().html())
+// 获取收货地址列表
+function getAddressList() {
+  ajaxPromise({ url: `${baseUrl}/wx/address/list` }).then(res => {
+    let temp = ''
+    res.data.forEach(item => {
+      if (item.isDefault) {
+        temp += `<li class="address-wrap default selected">
+        <div class="addressBox">
+          <div class="marker-par">
+            <i class="fa fa-location-arrow marker"></i>
+            <span class="marker-tip">寄送至</span>
+          </div>
+          <label for="addressInput" class="addressInfo" onclick="switchoverAddress(this)">
+            <span class="user-address">
+              ${item.province} ${item.city} ${item.district} ${item.addressDetail} ${item.addressName} ( ${item.name} 收 )
+              <em>${item.mobile}</em>
+            </span>
+          </label>
+        </div>
+      </li>`
+      } else {
+        temp += `<li class="address-wrap">
+        <div class="addressBox">
+          <label for="addressInput" class="addressInfo" onclick="switchoverAddress(this)">
+            <span class="user-address">
+              ${item.province} ${item.city} ${item.district} ${item.addressDetail} ${item.addressName} ( ${item.name} 收 )
+              <em>${item.mobile}</em>
+            </span>
+          </label>
+        </div>
+      </li>`
+      }
+    });
+    $('#address-list').html(temp)
+  })
+}
 
-          // $('.address-list label.addressInfo').attr('class', '')
+// 点击切换收货地址
+function switchoverAddress(e) {
+  let hasClass = $(e).parent().parent().hasClass('selected')
+  if (!hasClass) {
+    $.confirm({
+      title: '地址!',
+      content: '更换地址后, 您需要重新确认订单信息',
+      buttons: {
+        cancel: {
+          text: '取消'
+        },
+        confirm: {
+          text: '确认',
+          btnClass: 'btn-blue',
+          keys: ['enter', 'shift'],
+          action: function () {
+            $('.address-wrap').attr('class', 'address-wrap')
+            $(e).parent().parent().addClass('default selected')
+            $('.address-wrap .addressBox .marker-par').remove()
+            $(e).before(`<div class="marker-par">
+            <i class="fa fa-location-arrow marker"></i>
+            <span class="marker-tip">寄送至</span>
+          </div>`)
+            // console.log($(e).parent().parent().html())
+
+            // $('.address-list label.addressInfo').attr('class', '')
+          }
         }
       }
-    }
-  });
-})
+    });
+  }
+}
+
+// 获取购物车商品
+function getCartList() {
+  ajaxPromise({ url: `${baseUrl}/wx/cart/mall/index` }).then(res => {
+    $("#goods-data").html(template('cart_list', res.data))
+  })
+}
+
+// 修改
+function changeNumber(e) {
+  let number = e.target.value
+  let price = e.target.attributes.price.value
+  let cartId = e.target.attributes.cartId.value
+  $(`.goods-page-total[cartId=${cartId}]`).html(`<strong><span>¥</span>${Number(number) * Number(price)}</strong>`)
+  var length = $('.goods-page-total strong').length
+  var allTotal = 0
+  for (let i = 0; i < length; i++) {
+    allTotal += Number($('.goods-page-total strong')[i].innerText.trim().slice(1))
+  }
+  $('.shopping-total-price .price').html(`<span>¥</span>${allTotal}`)
+}
+
+getAddressList()
+getCartList()
